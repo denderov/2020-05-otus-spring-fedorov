09c56dd317e0bc9bd953c3c6053e3618e2339764
denderov <denderov@gmail.com>
2020-06-08 11:54:53 +0300

    removing old rebased classes.

--- hw02-annotation-based/src/main/java/ru/otus/homework/quiz/service/QuizTestService.java
+++
@@ -1,9 +0,0 @@
-package ru.otus.homework.quiz.service;
-
-import java.util.List;
-import ru.otus.homework.quiz.domain.QuizQuestion;
-
-public interface QuizTestService {
-
-  void runTest(List<QuizQuestion> quizQuestions);
-}
