aa52ff813b88b8ccb0890554da9098c8aa8006cc
denderov <denderov@gmail.com>
2020-06-12 14:52:41 +0300

    QuizTestingServiceImplTest refactoring.

--- hw02-annotation-based/src/test/java/ru/otus/homework/quiz/service/QuizTestingServiceImplTest.java
+++ hw02-annotation-based/src/test/java/ru/otus/homework/quiz/service/QuizTestingServiceImplTest.java
@@ -1,14 +1,8 @@
 package ru.otus.homework.quiz.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.BDDMockito.given;
 
-import java.io.BufferedReader;
-import java.io.ByteArrayOutputStream;
-import java.io.InputStreamReader;
-import java.io.PrintStream;
 import java.util.stream.Collectors;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -16,9 +10,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import ru.otus.homework.TestHelper;
 import ru.otus.homework.common.IOService;
-import ru.otus.homework.common.IOServiceImpl;
 import ru.otus.homework.config.QuizTestProperties;
-import ru.otus.homework.quiz.dao.QuizDao;
 import ru.otus.homework.quiz.domain.TestQuestion;
 
 @DisplayName("Класс QuizTestingServiceImpl")
@@ -28,30 +20,18 @@ class QuizTestingServiceImplTest {
   private static final int DEFAULT_TEST_QUESTIONS_COUNT = 5;
   private static final int DEFAULT_PASS_PERCENT = 80;
   @Mock
-  private static QuizDao quizDao;
-  private static ByteArrayOutputStream testOut;
   private static IOService ioService;
-  private static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-
-  @BeforeAll
-  static void initIO() {
-    testOut = new ByteArrayOutputStream();
-    ioService = new IOServiceImpl(reader, new PrintStream(testOut));
-  }
 
   @DisplayName("корректно создает комнату для тестов")
   @Test
   void shouldCorrectCreateTestRoom() {
-    given(quizDao.loadQuizItems()).willReturn(TestHelper.TEST_QUIZ_QUESTIONS);
     QuizTestProperties quizTestProperties = new QuizTestProperties(DEFAULT_TEST_QUESTIONS_COUNT,
         DEFAULT_PASS_PERCENT);
 
     QuizTestingServiceImpl quizTestService = new QuizTestingServiceImpl(
         ioService, quizTestProperties);
-    QuizServiceImpl quizService = new QuizServiceImpl(ioService, quizDao, quizTestService);
-    quizService.readQuiz();
     quizTestService.createTestRoom(TestHelper.FIRST_NAME, TestHelper.LAST_NAME,
-        quizService.getQuizQuestions());
+        TestHelper.TEST_QUIZ_QUESTIONS);
     assertThat(
         quizTestService.getTestRoom().getTestQuestions().stream().map(TestQuestion::getQuizQuestion)
             .collect(Collectors.toList()))
