40585e05ba7d2fdb55007222d70d943e3dde8600
naf <fsanek@yandex.ru>
2020-06-18 16:39:38 +0300

    test configuration example.

--- hw04-shell/src/test/java/ru/otus/homework/quiz/dao/QuizDaoCsvTest.java
+++ hw04-shell/src/test/java/ru/otus/homework/quiz/dao/QuizDaoCsvTest.java
@@ -7,7 +7,10 @@ import java.util.List;
 import java.util.Locale;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import ru.otus.homework.TestHelper;
 import ru.otus.homework.quiz.domain.QuizQuestion;
 
@@ -15,13 +18,16 @@ import ru.otus.homework.quiz.domain.QuizQuestion;
 @SpringBootTest
 class QuizDaoCsvTest {
 
+  @Autowired
+  QuizDao correctQuizDao;
+  @Autowired
+  QuizDao incorrectQuizDao;
+
   @DisplayName("корректно читает вопросы из ридера в QuizItem")
   @Test
   void shouldCorrectReadQuiz() {
 
-    QuizDao quizDao = new QuizDaoCsv("correct_test", Locale.US);
-
-    List<QuizQuestion> quizQuestionsFromDao = quizDao.loadQuizItems();
+    List<QuizQuestion> quizQuestionsFromDao = correctQuizDao.loadQuizItems();
 
     assertThat(quizQuestionsFromDao).isEqualTo(TestHelper.TEST_QUIZ_QUESTIONS);
 
@@ -31,15 +37,27 @@ class QuizDaoCsvTest {
   @Test
   void shouldCorrectThrowExceptionThenIncorrectCsvFormat() {
 
-    QuizDao quizDao = new QuizDaoCsv("incorrect_test", Locale.US);
-
-    Throwable thrown = catchThrowable(quizDao::loadQuizItems);
+    Throwable thrown = catchThrowable(incorrectQuizDao::loadQuizItems);
 
     assertThat(thrown).isInstanceOf(QuizLoadingException.class)
         .hasMessageContaining("Проверка четности. "
             + "Строка должна содержать вопрос и пары ответ - "
             + "флаг корректности ответа, разделенные запятыми.");
 
+  }
+
+  @Configuration
+  public static class TestConfig {
+
+    @Bean
+    QuizDao correctQuizDao() {
+      return new QuizDaoCsv("correct_test", Locale.US);
+    }
+
+    @Bean
+    QuizDao incorrectQuizDao() {
+      return new QuizDaoCsv("incorrect_test", Locale.US);
+    }
   }
 
 }
