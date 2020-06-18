2f08b061bc1e786219416cd96b45db1da5248ef9
naf <fsanek@yandex.ru>
2020-06-18 16:28:47 +0300

    clean up tests.

--- hw04-shell/src/test/java/ru/otus/homework/quiz/service/QuizServiceImplTest.java
+++ hw04-shell/src/test/java/ru/otus/homework/quiz/service/QuizServiceImplTest.java
@@ -10,10 +10,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.MessageSource;
 import ru.otus.homework.TestHelper;
 import ru.otus.homework.common.IOService;
-import ru.otus.homework.config.QuizProperties;
 import ru.otus.homework.quiz.dao.QuizDao;
 
 @DisplayName("Класс QuizServiceImpl")
@@ -24,10 +22,9 @@ class QuizServiceImplTest {
   private static IOService ioService;
   @MockBean
   private static QuizDao quizDao;
+
   @Autowired
-  private QuizProperties quizProperties;
-  @MockBean
-  private MessageSource messageSource;
+  private QuizService quizService;
 
   @DisplayName("корректно печатает вопросы, полученные из DAO")
   @Test
@@ -35,9 +32,6 @@ class QuizServiceImplTest {
 
     given(quizDao.loadQuizItems()).willReturn(TestHelper.TEST_QUIZ_QUESTIONS);
 
-    QuizTestingService quizTestingService = new QuizTestingServiceImpl(
-        ioService, quizProperties, messageSource);
-    QuizService quizService = new QuizServiceImpl(ioService, quizDao, quizTestingService);
     quizService.readQuiz();
     quizService.printQuizQuestions();
 
--- hw04-shell/src/test/java/ru/otus/homework/quiz/service/QuizTestingServiceImplTest.java
+++ hw04-shell/src/test/java/ru/otus/homework/quiz/service/QuizTestingServiceImplTest.java
@@ -7,30 +7,20 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.MessageSource;
 import ru.otus.homework.TestHelper;
-import ru.otus.homework.common.IOService;
-import ru.otus.homework.config.QuizProperties;
 import ru.otus.homework.quiz.domain.TestQuestion;
 
 @DisplayName("Класс QuizTestingServiceImpl")
 @SpringBootTest
 class QuizTestingServiceImplTest {
 
-  @MockBean
-  private static IOService ioService;
   @Autowired
-  private QuizProperties quizProperties;
-  @MockBean
-  private MessageSource messageSource;
+  private QuizTestingServiceImpl quizTestService;
 
   @DisplayName("корректно создает комнату для тестов")
   @Test
   void shouldCorrectCreateTestRoom() {
 
-    QuizTestingServiceImpl quizTestService = new QuizTestingServiceImpl(
-        ioService, quizProperties, messageSource);
     quizTestService.createTestRoom(TestHelper.FIRST_NAME, TestHelper.LAST_NAME,
         TestHelper.TEST_QUIZ_QUESTIONS);
     assertThat(
