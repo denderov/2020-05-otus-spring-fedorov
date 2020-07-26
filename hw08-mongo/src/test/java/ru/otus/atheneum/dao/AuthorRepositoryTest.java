1753b30c2c99dc86c5964dd034b736698e793b25
denderov <denderov@gmail.com>
2020-07-26 19:00:50 +0300

    prepare AuthorRepositoryTest

--- hw08-mongo/src/test/java/ru/otus/atheneum/dao/AuthorRepositoryTest.java
+++ hw08-mongo/src/test/java/ru/otus/atheneum/dao/AuthorRepositoryTest.java
@@ -1,17 +1,24 @@
 package ru.otus.atheneum.dao;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import java.util.Optional;
+import javax.validation.ConstraintViolationException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.annotation.DirtiesContext.ClassMode;
 import ru.otus.atheneum.TestHelper;
 import ru.otus.domain.Author;
 
 @DisplayName("Интерфейс AuthorRepository")
 @DataMongoTest
+//ну нет роллбэка в монге :(
+@DirtiesContext(classMode = ClassMode.AFTER_EACH_TEST_METHOD)
 @EnableConfigurationProperties
 class AuthorRepositoryTest {
 
@@ -25,6 +32,20 @@ class AuthorRepositoryTest {
     assertThat(actualAuthor).hasFieldOrPropertyWithValue("fullName", TestHelper.AUTHOR_FULL_NAME_1);
   }
 
+  @DisplayName("возвращает пустой Optional по несуществующему id")
+  @Test
+  void shouldReturnEmptyOptionalByMissingId() {
+    Optional<Author> author = authorRepository.findById("missingid");
+    assertThat(author).isEqualTo(Optional.empty());
+  }
+
+  @DisplayName("возвращает полный список авторов")
+  @Test
+  void shouldReturnAllAuthors() {
+    Iterable<Author> authors = authorRepository.findAll();
+    assertThat(authors).isEqualTo(TestHelper.AUTHORS);
+  }
+
   @DisplayName("добавляет автора")
   @Test
   void shouldInsertAuthor() {
@@ -33,6 +54,32 @@ class AuthorRepositoryTest {
     authorForSave.setFullName(fullName);
     Author testAuthor20200625 = authorRepository.save(authorForSave);
     assertThat(testAuthor20200625).hasFieldOrPropertyWithValue("fullName", fullName);
+  }
+
+  @DisplayName("удаляет автора и только его")
+  @Test
+  void shouldDeleteAuthor() {
+    authorRepository.delete(TestHelper.AUTHOR_3);
+    Iterable<Author> authors = authorRepository.findAll();
+    assertThat(authors).doesNotContain(TestHelper.AUTHOR_3)
+        .contains(TestHelper.AUTHOR_1, TestHelper.AUTHOR_2);
+  }
+
+  @DisplayName("не удаляет автора, если на него есть ссылка")
+  @Test
+  void shouldNotDeleteLinkedAuthor() {
+    assertThatThrownBy(() -> authorRepository.delete(TestHelper.AUTHOR_1))
+        .hasCauseInstanceOf(ConstraintViolationException.class);
+  }
+
+  @DisplayName("изменяет автора")
+  @Test
+  void shouldUpdateAuthor() {
+    Author authorFromDb = TestHelper.AUTHOR_1;
+    Author authorForUpdate = new Author(authorFromDb.getId(), authorFromDb.getFullName());
+    authorForUpdate.setFullName(TestHelper.AUTHOR_FULL_NAME_3);
+    Author actualAuthor = authorRepository.save(authorForUpdate);
+    assertThat(actualAuthor).hasFieldOrPropertyWithValue("fullName", TestHelper.AUTHOR_FULL_NAME_3);
   }
 
 }
--- hw08-mongo/src/test/java/ru/otus/changelog/InitMongoTestChangelog.java
+++ hw08-mongo/src/test/java/ru/otus/changelog/InitMongoTestChangelog.java
@@ -5,12 +5,9 @@ import com.github.cloudyrock.mongock.ChangeSet;
 import com.github.cloudyrock.mongock.driver.mongodb.springdata.v3.decorator.impl.MongockTemplate;
 import com.mongodb.client.MongoDatabase;
 import ru.otus.atheneum.TestHelper;
-import ru.otus.domain.Author;
 
 @ChangeLog(order = "001")
 public class InitMongoTestChangelog {
-
-  private Author author;
 
   @ChangeSet(order = "000", id = "dropDB", author = "fedorov", runAlways = true)
   public void dropDB(MongoDatabase database) {
@@ -19,6 +16,8 @@ public class InitMongoTestChangelog {
 
   @ChangeSet(order = "001", id = "initAuthors", author = "fedorov", runAlways = true)
   public void initAuthors(MongockTemplate template) {
-    author = template.save(TestHelper.AUTHOR_1);
+    template.save(TestHelper.AUTHOR_1);
+    template.save(TestHelper.AUTHOR_2);
+    template.save(TestHelper.AUTHOR_3);
   }
 }
