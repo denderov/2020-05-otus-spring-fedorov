6b3a4250092c2e7b3ec722e6e591e86f41b2f0bb
denderov <denderov@gmail.com>
2020-09-08 08:10:39 +0300

    edit book implementation.

--- hw10-modern-web/src/main/java/ru/otus/atheneum/controller/BookController.java
+++ hw10-modern-web/src/main/java/ru/otus/atheneum/controller/BookController.java
@@ -4,13 +4,14 @@ import java.util.List;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.view.RedirectView;
+import ru.otus.atheneum.dto.AuthorRow;
 import ru.otus.atheneum.dto.BookRow;
+import ru.otus.atheneum.dto.GenreRow;
 import ru.otus.atheneum.service.AuthorService;
 import ru.otus.atheneum.service.BookService;
 import ru.otus.atheneum.service.EntityConverter;
@@ -27,18 +28,50 @@ public class BookController {
   private final EntityConverter entityConverter;
 
   @GetMapping("/api/books")
-  public List<BookRow> showBookList(Model model) {
+  public List<BookRow> showBookList() {
     List<BookRow> books = bookService.getAll().stream().map(entityConverter::convertBookEntityToDto)
         .collect(Collectors.toList());
     log.info(books.toString());
     return books;
   }
 
+  @GetMapping("/api/book/{id}")
+  public BookRow showBookById(@PathVariable("id") String id) {
+    BookRow book = entityConverter.convertBookEntityToDto(bookService.getById(id).orElseThrow());
+    log.info(String.format("Возврат книги с id = %s", id));
+    return book;
+  }
+
+  @GetMapping("/api/authors")
+  public List<AuthorRow> showAuthors() {
+    List<AuthorRow> authors = authorService.getAll().stream()
+        .map(entityConverter::convertAuthorEntityToDto)
+        .collect(Collectors.toList());
+    log.info(authors.toString());
+    return authors;
+  }
+
+  @GetMapping("/api/genres")
+  public List<GenreRow> showGenres() {
+    List<GenreRow> genres = genreService.getAll().stream()
+        .map(entityConverter::convertGenreEntityToDto)
+        .collect(Collectors.toList());
+    log.info(genres.toString());
+    return genres;
+  }
+
   @DeleteMapping("/api/book/{id}")
-  public RedirectView deleteBookById(Model model, @PathVariable("id") String id) {
+  public String deleteBookById(@PathVariable("id") String id) {
     bookService.delete(id);
     log.info(String.format("Удаление книги с id = %s", id));
-    return new RedirectView("/", true);
+    return "Book deleted";
+  }
+
+  @PostMapping("/api/book")
+  public String saveBook(BookRow bookRow) {
+    bookService.update(entityConverter.convertBookDtoToEntity(bookRow));
+    log.info(String.format("Сохранение книги %s", bookRow));
+    return "Book saved";
   }
 //
 //  @GetMapping("/book/edit/{id}")
--- hw10-modern-web/src/main/resources/static/js/app.js
+++ hw10-modern-web/src/main/resources/static/js/app.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
 
+
   $.get(
       "/api/books",
       showAllBooks
@@ -8,24 +9,25 @@ $(document).ready(function () {
 
 function showAllBooks(json) {
 
+  $('#tableContent').empty();
+
   // Get table body and print
   for (var i = 0; i < Object.keys(json).length; i++) {
-    $('#table_content').append(
-        '<tr id="' + json[i][Object.keys(json[0])[0]] + '">'
-        + '    <td>' + json[i][Object.keys(json[0])[1]] + '</td>\n'
-        + '    <td>' + json[i][Object.keys(json[0])[3]] + '</td>\n'
-        + '    <td>' + json[i][Object.keys(json[0])[5]] + '</td>\n'
+    $('#tableContent').append(
+        '<tr id="' + json[i]["id"] + '">'
+        + '    <td>' + json[i]["title"] + '</td>\n'
+        + '    <td>' + json[i]["author"] + '</td>\n'
+        + '    <td>' + json[i]["genre"] + '</td>\n'
         + '    <td>\n'
         + '      <div class="dropdown dropleft">\n'
         + '        <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown">\n'
         + '          Action\n'
         + '        </button>\n'
         + '        <div class="dropdown-menu">\n'
-        + '          <a class="dropdown-item" href="/api/book/edit/'
-        + json[i][Object.keys(json[0])[0]] + '"\n'
-        + '              href="book_edit.html">Edit</a>\n'
-        + '          <div class="delete-book" id="' + json[i][Object.keys(
-        json[0])[0]] + '">\n'
+        + '          <div class="edit-book" id="' + json[i]["id"] + '">\n'
+        + '            <a class="dropdown-item" href="#">Edit</a>\n'
+        + '          </div>\n'
+        + '          <div class="delete-book" id="' + json[i]["id"] + '">\n'
         + '            <a class="dropdown-item" href="#">Delete</a>\n'
         + '          </div>\n'
         + '        </div>\n'
@@ -33,7 +35,20 @@ function showAllBooks(json) {
         + '    </td>\n'
         + '</tr>'
     );
+
+    $(".bookTable").show();
   }
+
+  $(".edit-book").click(function () {
+    var book_id = $(this).attr('id');
+    $(".bookTable").hide();
+    showEditor(book_id);
+  });
+
+  $(".new-book").click(function () {
+    $(".bookTable").hide();
+    showEditor(null);
+  });
 
   $(".delete-book").click(function () {
     var book_id = $(this).attr('id');
@@ -42,7 +57,53 @@ function showAllBooks(json) {
       type: "DELETE"
     })
     $(this).closest("tr").remove();
-    // $('#table_content').find("tr#" + book_id).remove();
   });
 
+}
+
+function showEditor(book_id) {
+  $.get(
+      "/api/book/" + book_id,
+      fillBookEditor
+  );
+  $('#editForm').show();
+}
+
+function fillBookEditor(json) {
+  $('#id').empty().attr("value", json["id"]);
+  $('#title').empty().attr("value", json["title"]);
+  $('#author').empty().attr("value", json["authorId"]);
+  $('#genre').empty().attr("value", json["genreId"]);
+
+  $.get(
+      "/api/authors",
+      function (authorJson) {
+        for (var i = 0; i < Object.keys(authorJson).length; i++) {
+          $('#author').append('<option value="' + authorJson[i]["id"] + '">'
+              + authorJson[i]["fullName"] + '</option>');
+        }
+      }
+  );
+
+  $.get(
+      "/api/genres",
+      function (genreJson) {
+        for (var i = 0; i < Object.keys(genreJson).length; i++) {
+          $('#genre').append('<option value="' + genreJson[i]["id"] + '">'
+              + genreJson[i]["name"] + '</option>');
+        }
+      }
+  );
+
+  $('#editForm').submit(function (e) {
+    e.preventDefault();
+    $.post("api/book",
+        $(this).serialize());
+    $('#editForm').hide();
+
+    $.get(
+        "/api/books",
+        showAllBooks
+    );
+  });
 }
