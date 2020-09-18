04bca7741b0974396b3b203aefde25a9284d78a5
denderov <denderov@gmail.com>
2020-09-18 07:20:44 +0300

    controller unit test.

--- hw10-modern-web/src/test/java/ru/otus/atheneum/controller/BookControllerUnitTest.java
+++ hw10-modern-web/src/test/java/ru/otus/atheneum/controller/BookControllerUnitTest.java
@@ -1,12 +1,19 @@
 package ru.otus.atheneum.controller;
 
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.modelmapper.ModelMapper;
 import org.springframework.ui.Model;
+import ru.otus.atheneum.TestHelper;
 import ru.otus.atheneum.service.AuthorService;
 import ru.otus.atheneum.service.BookService;
+import ru.otus.atheneum.service.EntityConverterImpl;
 import ru.otus.atheneum.service.GenreService;
 
 //@EnableAutoConfiguration(exclude = {MongockConfiguration.class, MongockTestContext.class})
@@ -26,14 +33,15 @@ public class BookControllerUnitTest {
   @Mock
   private Model model;
 
-//  @Test
-//  @DisplayName("корректно вызывает удаление книги")
-//  void shouldCorrectDeleteBook() {
-//    BookController bookController = new BookController(bookService, authorService,
-//        genreService, new EntityConverterImpl(authorService, genreService, new ModelMapper()));
-//    String test_id = TestHelper.BOOK_1.getId();
-//    bookController.deleteBookById(model, test_id);
-//    verify(bookService, times(1))
-//        .delete(test_id);
-//  }
+  @Test
+  @DisplayName("корректно вызывает удаление книги")
+  void shouldCorrectDeleteBook() {
+    BookController bookController = new BookController(bookService, authorService,
+        genreService, new EntityConverterImpl(authorService, genreService, new ModelMapper()));
+    String test_id = TestHelper.BOOK_1.getId();
+    bookController.deleteBookById(test_id);
+    verify(bookService, times(1))
+        .delete(test_id);
+  }
 }
+
