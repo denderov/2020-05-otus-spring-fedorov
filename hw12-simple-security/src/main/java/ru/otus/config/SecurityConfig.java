69e1c12113e3af218cccca5d13a20f032c27ae59
denderov <denderov@gmail.com>
2020-10-07 07:37:56 +0300

    simple security configuration

---
+++ hw12-simple-security/src/main/java/ru/otus/config/SecurityConfig.java
@@ -0,0 +1,52 @@
+package ru.otus.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+//  @Autowired
+//  private AccessDeniedHandler accessDeniedHandler;
+
+  @Override
+  public void configure(WebSecurity web) {
+    web.ignoring().antMatchers("/", "/webjars/**");
+  }
+
+  @Override
+  public void configure(HttpSecurity http) throws Exception {
+    http.csrf().disable()
+        .authorizeRequests()
+        .antMatchers("/**").hasAnyRole("ADMIN")
+        .anyRequest().authenticated()
+        .and()
+        .formLogin()
+//        .successForwardUrl("/success").failureForwardUrl("/error")
+//        .loginPage("/login")
+//        .permitAll()
+        .and()
+        .logout()
+        .permitAll();
+  }
+
+  @SuppressWarnings("deprecation")
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return NoOpPasswordEncoder.getInstance();
+  }
+
+  @Autowired
+  public void configure(AuthenticationManagerBuilder auth) throws Exception {
+    auth.
+        inMemoryAuthentication()
+        .withUser("admin").password("password").roles("ADMIN");
+  }
+}
