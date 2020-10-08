bf70dbb3a33bd2d3cbf70ebd72b4a6fe00545d8b
denderov <denderov@gmail.com>
2020-10-08 07:49:04 +0300

    testing

--- hw12-simple-security/src/test/java/ru/otus/atheneum/controller/BookControllerTest.java
+++ hw12-simple-security/src/test/java/ru/otus/atheneum/controller/BookControllerTest.java
@@ -4,6 +4,7 @@ import static org.hamcrest.Matchers.containsString;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrlPattern;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.jupiter.api.DisplayName;
@@ -13,6 +14,8 @@ import org.springframework.boot.test.autoconfigure.data.mongo.AutoConfigureDataM
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
+import org.springframework.security.test.context.support.WithAnonymousUser;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import ru.otus.atheneum.TestHelper;
 import ru.otus.atheneum.service.AuthorService;
@@ -20,11 +23,13 @@ import ru.otus.atheneum.service.BookService;
 import ru.otus.atheneum.service.EntityConverter;
 import ru.otus.atheneum.service.EntityConverterImpl;
 import ru.otus.atheneum.service.GenreService;
+import ru.otus.atheneum.service.MongoUserService;
+import ru.otus.config.SecurityConfig;
 import ru.otus.config.WebConfig;
 
 @WebMvcTest(value = BookController.class)
 @AutoConfigureDataMongo
-@Import({EntityConverterImpl.class, WebConfig.class})
+@Import({EntityConverterImpl.class, WebConfig.class, SecurityConfig.class, MongoUserService.class})
 @DisplayName("Класс BookController ")
 public class BookControllerTest {
 
@@ -43,6 +48,9 @@ public class BookControllerTest {
   @Autowired
   private EntityConverter entityConverter;
 
+  @Autowired
+  private MongoUserService mongoUserService;
+
   @Test
   @DisplayName("корректно отражает список книг")
   void shouldCorrectShowBooks() throws Exception {
@@ -55,5 +63,25 @@ public class BookControllerTest {
         .andExpect(content().string(containsString("Test_book_2")))
         .andExpect(content().string(containsString("Test_author_2")))
         .andExpect(content().string(containsString("Test_genre_2")));
+  }
+
+  @DisplayName("разрешает редактирование для авторизованных")
+  @WithMockUser(authorities = {"USER", "ADMIN"})
+  @Test
+  void allowsEditForAdmin() throws Exception {
+    given(genreService.getAll()).willReturn(TestHelper.GENRES);
+    given(authorService.getAll()).willReturn(TestHelper.AUTHORS);
+    mvc.perform(get("/book/edit")).andExpect(status().isOk());
+  }
+
+  @DisplayName("запрещает редактирование для анонимов")
+  @WithAnonymousUser
+  @Test
+  void disableEditForAdmin() throws Exception {
+    given(genreService.getAll()).willReturn(TestHelper.GENRES);
+    given(authorService.getAll()).willReturn(TestHelper.AUTHORS);
+    mvc.perform(get("/book/edit")).andExpect(status().is3xxRedirection())
+        .andExpect(redirectedUrlPattern("**/login"))
+    ;
   }
 }
--- hw12-simple-security/src/test/java/ru/otus/testchangelog/InitMongoTestChangelog.java
+++ hw12-simple-security/src/test/java/ru/otus/testchangelog/InitMongoTestChangelog.java
@@ -5,6 +5,7 @@ import com.github.cloudyrock.mongock.ChangeSet;
 import com.github.cloudyrock.mongock.driver.mongodb.springdata.v3.decorator.impl.MongockTemplate;
 import com.mongodb.client.MongoDatabase;
 import ru.otus.atheneum.TestHelper;
+import ru.otus.domain.Users;
 
 @ChangeLog(order = "001")
 public class InitMongoTestChangelog {
@@ -12,6 +13,12 @@ public class InitMongoTestChangelog {
   @ChangeSet(order = "000", id = "dropDB", author = "fedorov", runAlways = true)
   public void dropDB(MongoDatabase database) {
     database.drop();
+  }
+
+  @ChangeSet(order = "0001", id = "initUsers", author = "fedorov", runAlways = true)
+  public void initUsers(MongockTemplate template) {
+    template.save(new Users("admin", "admin", "ADMIN"));
+    template.save(new Users("user", "user", "USER"));
   }
 
   @ChangeSet(order = "001", id = "initAuthors", author = "fedorov", runAlways = true)
